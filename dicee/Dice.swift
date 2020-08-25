ae8f139e2f20991fe6e8e53524fc0ae01a1bb875
Bharath Raj Kumar <bharath@incend.in>
2020-08-25 16:19:32 +0530

    Converted to MVVM

---
+++ dicee/Dice.swift
@@ -0,0 +1,15 @@
+//
+//  Dice.swift
+//  dicee
+//
+//  Created by Bharath  Raj kumar on 25/08/20.
+//  Copyright © 2020 Bharath Raj Kumar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Dice {
+    var diceImage: UIImage
+    var diceName: String
+}
--- dicee/ViewController.swift
+++ dicee/ViewController.swift
@@ -10,67 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var randomDiceIndex1 : Int =  0
-    var randomDiceIndex2 : Int =  0
-    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6" ]
-    
     @IBOutlet weak var rollCount: UILabel!
     @IBOutlet weak var diceText1: UILabel!
     @IBOutlet weak var diceText2: UILabel!
-    let textArray = ["One","Two","Three","Four","Five","Six"]
-    private var rollCount1 = 0
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let viewModel = DiceViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        updateDiceImage()
         
+        viewModel.delegate = self
+        viewModel.updateDice(state: .update)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
+    
     @IBAction func rollButtonPressed(_ sender: Any) {
         
-        updateDiceImage()
-        
-        
-        rollCount1 = rollCount1 + 1
-        rollCount.text = "\(rollCount1)"
-        }
+        viewModel.updateDice(state: .update)
+    }
     @IBAction func restbutton(_ sender: Any) {
-        randomDiceIndex1 = 0
-        randomDiceIndex2 = 0
-        rollCount1 = 0
-        
-        rollCount.text = "\(rollCount1)"
-        diceText1.text = textArray[0]
-        diceText2.text = textArray[0]
         
         let buttonTapped = UIAlertController(title: "Reset", message: "Do you want to reset?", preferredStyle: .alert)
-        buttonTapped.addAction(UIAlertAction(title: "OK", style: .default , handler: nil))
+        
+        let okAlertAction = UIAlertAction(title: "OK", style: .default) { [weak self] (_) in
+            self?.viewModel.updateDice(state: .reset)
+        }
+        
+        buttonTapped.addAction(okAlertAction)
         
         self.present(buttonTapped,animated: true, completion: nil)
     }
     
-    func updateDiceImage() {
-        randomDiceIndex1 = Int(arc4random_uniform(6))
-        randomDiceIndex2 = Int(arc4random_uniform(6))
-        
-        
-        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
-        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-        
-        diceText1.text = textArray[randomDiceIndex1]
-        diceText2.text = textArray[randomDiceIndex2]
-    }
-    
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-        updateDiceImage()
+        viewModel.updateDice(state: .update)
     }
     
 }
 
+extension ViewController: DiceRollDelegate {
+    
+    func updateDiceImage(diceOne: Dice, diceTwo: Dice) {
+        diceImageView1.image = diceOne.diceImage
+        diceImageView2.image = diceTwo.diceImage
+        
+        diceText1.text = diceOne.diceName
+        diceText2.text = diceTwo.diceName
+    }
+    
+    func updateRoll(rollCount: Int) {
+        self.rollCount.text = "\(rollCount)"
+    }
+}
